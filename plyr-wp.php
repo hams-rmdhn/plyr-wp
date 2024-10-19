979ef995acdf48c298088c0638816289ee2e66e2
Ilham Wahyudin <hams.rmdhn@gmail.com>
2024-10-19 22:38:21 +0700

    Add: autoplayer control

--- plyr-wp.php
+++ plyr-wp.php
@@ -14,8 +14,9 @@ add_shortcode( 'plyr-wp', 'plyr' );
 function plyr( $atts ) {
 	
 	$a = shortcode_atts( array(
-		'url' => '',
-		'color' => ''
+		'url' 		=> '',
+		'color' 	=> '',
+		'autoplay' 	=> ''
 	), $atts );
 
 	// Get URL	
@@ -24,7 +25,9 @@ function plyr( $atts ) {
 	$youtube_id = $match[1];
 
 	// Get Attributes
-	$color = !empty($a['color']) ? esc_attr($a['color']) : '#FF0000';
+	$color 		= !empty($a['color']) ? esc_attr($a['color']) : '#FF0000';
+	$autoplay 	= !empty($a['autoplay']) ? esc_attr($a['autoplay']) : 'false';
+	$autoplyr 	= $autoplay == 'true' ? '1' : '0';
 
 	// Returned
 	$output = '
@@ -32,12 +35,12 @@ function plyr( $atts ) {
 		<link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
 		<script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>
 		
-		<div class="video" id="player" style="--plyr-color-main: '. $color .';" data-plyr-config="{ \'autoplay\': \'true\' }" data-plyr-provider="youtube" data-plyr-embed-id="' . $youtube_id .'"></div>
+		<div class="video" id="player" style="--plyr-color-main: '. $color .';" data-plyr-config="{ \'autoplay\': \''.$autoplay.'\' }" data-plyr-provider="youtube" data-plyr-embed-id="' . $youtube_id .'"></div>
 		
 		<script>
 			const player = new Plyr("#player", {
-				youtube: 	{ noCookie:false, rel: 0, showinfo: 0, iv_load_policy: 3, modestbranding: 1, autoplay: 1 },
-				autoplay: 	true
+				youtube:	{ noCookie:false, rel: 0, showinfo: 0, iv_load_policy: 3, modestbranding: 1, autoplay: '.$autoplyr.' },
+				autoplay:	'.$autoplay.'
 			});
 			window.player = player;
 		</script>
